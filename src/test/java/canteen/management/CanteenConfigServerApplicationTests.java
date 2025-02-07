06a026a87db56b044fe7f2e87e5d9268d7c32b71
ketand <ketand@cdac.in>
2025-02-07 14:40:10 +0530

    initial Commit

---
+++ src/test/java/canteen/management/CanteenConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package canteen.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanteenConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
